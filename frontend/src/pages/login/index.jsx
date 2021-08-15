b415713c5bec86f8421661136fa9fe3058d7b61c
wang <yanceyy@users.noreply.github.com>
2021-08-15 14:18:44 +1000

    login page

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -7,9 +7,27 @@ function LoginPage() {
             <div className="loginWrapper">
                 <div className="loginLeft">
                     <h3 className="loginLogo">SocialWhat</h3>
-                    <span className="loginDesc"></span>
+                    <span className="loginDesc">Embrace the World</span>
                 </div>
-                <div className="loginRight"></div>
+                <div className="loginRight">
+                    <div className="loginBox">
+                        <input
+                            type="text"
+                            placeholder="Email"
+                            id="LoginUsername"
+                        />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            id="LoginPassword"
+                        />
+                        <button className="LoginButton">Login</button>
+                        <div className="loginForget">Forget password?</div>
+                        <button className="loginRegisterButton">
+                            Create a New account
+                        </button>
+                    </div>
+                </div>
             </div>
         </div>
     );
