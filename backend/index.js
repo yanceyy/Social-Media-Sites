1623c97f475f605b3a1ba99f3575adae770bc99e
wang <yanceyy@users.noreply.github.com>
2021-08-30 21:59:22 +1000

    fix CSP

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const cors = require('cors');
 dotenv.config();
 
 const app = express();
-
+app.use(express.static(path.join(__dirname, "/frontend/build")));
 app.use(cors());
 app.use("/images", express.static(path.join(__dirname, "public/images")))
 // Connect to mongodb service
@@ -45,7 +45,14 @@ app.post('/api/upload', upload.single("file"), (req, res) => {
 // middleware
 app.use(express.json());
 app.use(express.urlencoded());
-app.use(helmet());
+app.use(helmet({
+    contentSecurityPolicy: {
+        directives: {
+            ... helmet.contentSecurityPolicy.getDefaultDirectives(),
+            "script-src": ["'self'", "'unsafe-inline'", "herokuapp.com"]
+        }
+    }
+}));
 app.use(morgan("common"));
 
 
@@ -53,7 +60,9 @@ app.use("/api/user", userRoute)
 app.use("/api/auth", authRoute)
 app.use("/api/post", postRoute)
 app.use("/api/comment", commentRoute)
-
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/build', 'index.html'));
+});
 
 app.listen(process.env.PORT || 8800, () => {
     console.log("server is running");
